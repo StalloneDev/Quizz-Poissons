f19cf82ca4a05aaf9efaa98923963d1d5cea8d90
Hantsaniala Eléo <hantsaniala@gmail.com>
2024-12-03 13:34:58 +0000

    fix: get values from .env

--- quiz_poissons/settings.py
+++ quiz_poissons/settings.py
@@ -12,13 +12,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # ALLOWED_HOSTS = ['quizz-poissons.srv653590.hstgr.cloud', 'localhost', '127.0.0.1', '*']
 
-ALLOWED_HOSTS = ["quizz-poissons.onrender.com"]
+ALLOWED_HOSTS = [os.environ["ALLOWED_HOST"],]
 # ALLOWED_HOSTS = []
 
 # Application definition
